a3c49d4a3ff05272e14599ccb1b1a2b32997b9cd
shivansh9794 <mshivansh549@gmail.com>
2025-04-01 13:14:53 +0530

    restarted work

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
-const url="mongodb+srv://Shivansh9794:Shivansh9794@shivansh.uehy1.mongodb.net/ShivanshProject?retryWrites=true&w=majority&appName=Shivansh"
-
+const url=process.env.Mongo_url;
 
 mongoose.connect(url).then((result) => {
     console.log("Connected to the database");
